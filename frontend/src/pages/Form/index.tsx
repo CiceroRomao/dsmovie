8144e2b8c0f53561e0abaf4533ed53d23eb39638
Cícero Romão <romaofilho500@gmail.com>
2022-01-13 11:41:25 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
-function Form(){
+function Form() {
     const movie = {
         id: 1,
         image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
@@ -8,7 +9,7 @@ function Form(){
         count: 2,
         score: 4.5
     };
-    return(
+    return (
         <div className="dsmovie-form-container">
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
@@ -32,7 +33,9 @@ function Form(){
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
                 </form >
-                <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                <Link to="/">
+                    <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+                </Link>
             </div >
         </div >
     );
